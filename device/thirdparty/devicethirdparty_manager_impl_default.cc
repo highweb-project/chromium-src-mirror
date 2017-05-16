c1d811de5abeb0680ece7b190c3617c683622196
Rand D. Kim <rand.d.kim@selvas.com>
2017-05-16 13:56:22 +0900

    ubuntu webd2d test code

--- device/thirdparty/devicethirdparty_manager_impl_default.cc
+++ device/thirdparty/devicethirdparty_manager_impl_default.cc
@@ -7,6 +7,9 @@
 
 #include "mojo/public/cpp/bindings/strong_binding.h"
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 namespace device {
 
 class DeviceThirdpartyManagerEmptyImpl : public DeviceThirdpartyManager {
@@ -15,11 +18,36 @@ class DeviceThirdpartyManagerEmptyImpl : public DeviceThirdpartyManager {
      : binding_(mojo::MakeStrongBinding(base::WrapUnique(this), std::move(request))) {}
   ~DeviceThirdpartyManagerEmptyImpl() override {}
 
-  void sendAndroidBroadcast(const std::string& action, const sendAndroidBroadcastCallback& callback) override {
-    SendAndroidBroadcastCallbackDataPtr result = SendAndroidBroadcastCallbackData::New();
-    result->action = "";
-    callback.Run(result.Clone());
-  }
+    void sendAndroidBroadcast(const std::string & action, const sendAndroidBroadcastCallback & callback) override {
+      LOG(ERROR) << "[3rd-party API][" << __FUNCTION__ << "][" << __LINE__ << "] action : " << action;
+      SendAndroidBroadcastCallbackDataPtr result = SendAndroidBroadcastCallbackData::New();
+      result->action = "ERROR";
+
+      const char* IP = "127.0.0.1";
+      int PORT = 8888;
+
+      int sock;
+      struct sockaddr_in server;
+      char server_reply[2000] = { 0, };
+      sock = socket(AF_INET , SOCK_STREAM , 0);
+      if (sock != -1) {
+        server.sin_addr.s_addr = inet_addr(IP);
+        server.sin_family = AF_INET;
+        server.sin_port = htons(PORT);
+
+        if (connect(sock , (struct sockaddr *)&server , sizeof(server)) == 0) {
+          if (send(sock , action.c_str() , strlen(action.c_str()) , 0) > 0) {
+            if (recv(sock , server_reply , 2000 , 0) > 0) {
+              std::string reply(server_reply);
+              result->action = reply;
+              LOG(ERROR) << "[3rd-party API][" << __FUNCTION__ << "][" << __LINE__ << "] reply : " << reply;
+            }
+          }
+          close(sock);
+        }
+        callback.Run(result.Clone());
+      }
+    }
 
  private:
   friend DeviceThirdpartyManagerImpl;
